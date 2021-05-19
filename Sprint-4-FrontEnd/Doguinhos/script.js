36f7bf1407d1fa56e43e2bc492308fa457195f82
lucasnauberth28 <lucasnauberth@gmail.com>
2021-05-19 12:17:12 -0300

    ufa

---
+++ Sprint-4-FrontEnd/Doguinhos/script.js
@@ -0,0 +1,30 @@
+dogs = [
+    {
+        nome: 'Sammy',
+        idade: 6,
+        raca: 'Samoieda'
+    },
+    {
+        nome: 'Dragão',
+        idade: 5,
+        raca: 'Spitz Alemão'
+    },
+    {
+        nome: 'Milton',
+        idade: 8,
+        raca: 'Spitz Alemão'
+    },
+    {
+        nome: 'Penny',
+        idade: 6,
+        raca: 'Spitz Alemão'
+    },
+    {
+        nome: 'Nenem',
+        idade: 4,
+        raca: 'Spitz Alemão'
+    },
+    ];
+
+
+    
