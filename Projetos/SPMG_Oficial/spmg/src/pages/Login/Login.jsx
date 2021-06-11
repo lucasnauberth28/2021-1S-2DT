efeaaebf09a1ef4f2f201d321572763d59178db6
LUCAS NAUBERTH SOUZA PROTASIO <00001078446374SP@aluno.educacao.sp.gov.br>
2021-06-11 16:31:15 -0300

    ufa

--- Projetos/SPMG_Oficial/spmg/src/pages/Login/Login.jsx
+++ Projetos/SPMG_Oficial/spmg/src/pages/Login/Login.jsx
@@ -1,5 +1,8 @@
-import '../../assets/Login.css';
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import '../../assets/Login.css';
+
 import Logo from '../../Images/Logo.svg'
 import Medico from '../../Images/Medico.png'
 
@@ -12,6 +15,22 @@ class Login extends Component{
         }
     };
 
+    fazerLogin = (evento) => {
+        evento.preventDefault();
+
+        this.setState({ erroMensagem : '', isLoading : true});
+
+        axios.post('http://localhost:5000/api/login', {
+            email : this.state.email,
+            senha : this.state.senha
+
+        })
+    }
+
+    atualizaCampoLogin = (campo) => {
+        this.setState({ [campo.target.name] : campo.target.value })
+    };
+
     render(){
         return(
             <main>
@@ -20,19 +39,22 @@ class Login extends Component{
                         <img src= {Medico} alt="Logo SPMG" id="foto"/>
                     </div>
                     <div id="login">
-                        <form action="submit" id="form-login">
+                        <form action="submit" id="form-login" >
                             <h2>Login</h2>
                             <input type="email" 
                             name="email" 
                             placeholder="Email" 
                             id="info-login" 
-                            value={this.state.email}>
+                            value={this.state.email}
+                            onChange={this.atualizaCampoLogin}>
                             </input>
-
-                            <input type="password" 
+ 
+                            <input type="password"
+                            name="senha" 
                             placeholder="Senha" 
-                            id="info-login">
-                            
+                            id="info-login"
+                            value={this.state.senha}
+                            onChange={this.atualizaCampoLogin}>
                             </input>
 
                             <input type="button" value="Entrar" id="btn"></input>
